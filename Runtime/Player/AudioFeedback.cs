084bee41967e1e5a160dc456833a6e1d335e8207
jonathan_aykroyd <byyjoww@gmail.com>
2022-07-02 15:00:16 +0100

    add audio feedback

---
+++ Runtime/Player/AudioFeedback.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Elysium.Audio
+{
+    public class AudioFeedback
+    {
+        [SerializeField] private AudioClip audioClip = default;
+    }
+}
